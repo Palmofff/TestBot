6318e9688dff9e7c6dbf5ce5ba0b21b5d21af58d
Palmofff <chernishenko2000@gmail.com>
2023-08-08 23:29:06 +0300

    Added get_wallet_transaction

--- main.py
+++ main.py
@@ -1,6 +1,9 @@
+import datetime
 import telebot
+import requests
 
-token = 'tg_toker'
+token = 'tg_token'
+bscan_api_key = 'bscan_api_key'
 
 bot = telebot.TeleBot(token)
 
@@ -9,5 +12,21 @@ def send_instructions(message):
     instructions = {"Привет! Отправь мне адрес кошелька на Binance ,"+
           "и я покажу последние 10 транзакций этого кошелька."}
     bot.send_message(message.chat.id, instructions)
+
+@bot.message_handler(func=lambda message: True)
+def get_wallet_transactions(message):
+    wallet_address = message.text.strip()
+
+    url = f"https://api.bscscan.com/api?module=account&action=txlist&address={wallet_address}&apikey={bscan_api_key}"
+    response = requests.get(url)
+    transactions = response.json().get('result', [])
+
+    transaction_list = []
+    for txn in transactions[-10:]:
+        txn_hash = txn['hash']
+        timestamp = datetime.utcfromtimestamp(int(txn['timeStamp'])).strftime('%Y-%m-%d %H:%M:%S')
+        transaction_list.append(f"Транзакция: [{txn_hash}]({f'https://bscscan.com/tx/{txn_hash}'}), Время: {timestamp}")
+
+    bot.send_message(message.chat.id, "\n".join(transaction_list), parse_mode='Markdown')
 
 bot.polling()
