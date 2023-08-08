b90e9f1b640fe4fa9540dc98653ea95c0a8c338b
Palmofff <chernishenko2000@gmail.com>
2023-08-08 23:33:41 +0300

    Added try-except blocks to get_wallet_transaction

--- main.py
+++ main.py
@@ -15,18 +15,21 @@ def send_instructions(message):
 
 @bot.message_handler(func=lambda message: True)
 def get_wallet_transactions(message):
-    wallet_address = message.text.strip()
+    try:
+        wallet_address = message.text.strip()
 
-    url = f"https://api.bscscan.com/api?module=account&action=txlist&address={wallet_address}&apikey={bscan_api_key}"
-    response = requests.get(url)
-    transactions = response.json().get('result', [])
+        url = f"https://api.bscscan.com/api?module=account&action=txlist&address={wallet_address}&apikey={bscan_api_key}"
+        response = requests.get(url)
+        transactions = response.json().get('result', [])
 
-    transaction_list = []
-    for txn in transactions[-10:]:
-        txn_hash = txn['hash']
-        timestamp = datetime.utcfromtimestamp(int(txn['timeStamp'])).strftime('%Y-%m-%d %H:%M:%S')
-        transaction_list.append(f"Транзакция: [{txn_hash}]({f'https://bscscan.com/tx/{txn_hash}'}), Время: {timestamp}")
+        transaction_list = []
+        for txn in transactions[-10:]:
+            txn_hash = txn['hash']
+            timestamp = datetime.utcfromtimestamp(int(txn['timeStamp'])).strftime('%Y-%m-%d %H:%M:%S')
+            transaction_list.append(f"Транзакция: [{txn_hash}]({f'https://bscscan.com/tx/{txn_hash}'}), Время: {timestamp}")
 
-    bot.send_message(message.chat.id, "\n".join(transaction_list), parse_mode='Markdown')
+        bot.send_message(message.chat.id, "\n".join(transaction_list), parse_mode='Markdown')
+    except Exception as error:
+        bot.send_message(message.chat.id, "Произошла ошибка! " + str(error))
 
 bot.polling()
